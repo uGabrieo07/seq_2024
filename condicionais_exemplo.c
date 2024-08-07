e48074d294efb3dbcfe887f356401d9647be425b
Gabriel Souza <gabrielsouza072004@gmail.com>
2024-08-07 19:20:01 -0300

    .
    .

---
+++ condicionais_exemplo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int numero = 10;
+
+    if (numero > 0) {
+        printf("O numero e positivo.\n");
+    }
+
+    return 0;
+}
+
+/* fazer exemplos para if/else, else if e switch */
