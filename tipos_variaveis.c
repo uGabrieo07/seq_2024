f7d7b3d7e84caac04d7ad1b24fa8fe07ff487506
Gabriel Souza <gabrielsouza072004@gmail.com>
2024-08-08 18:54:10 -0300

    .
    .

---
+++ tipos_variaveis.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define PI 3.14159;
+#define DIAS_DA_SEMANA 7;
+
+int main() {
+    int idade = 20;       /* Declara e inicializa 'idade' */
+    float altura = 1.80;  /* Declara e inicializa 'altura' */
+    char *nome = "Robson";     /* Declara e inicializa 'nome' */
+    char tipoSanguineo = 'B'; /* Declara e inicializa 'tipoSanguineo' */
+    const char inicial = 'A';
+    const int horasDia = 24;
+
+
+    /*Informacoes pessoais*/
+    printf("Nome: %s\n", nome);
+    printf("Idade: %d\n", idade);
+    printf("Altura: %.2f\n", altura);
+    printf("Tipo sanguineo: %c\n", tipoSanguineo);
+
+    /* Constantes e Macros*/
+
+    return 0;
+}
